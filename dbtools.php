195bacc2ccdeb95c65f49a9d45e49645b65b197a
Robert Labrie <robert.labrie@gmail.com>
2013-10-24 20:18:02 -0400

    db tools

---
+++ dbtools.php
@@ -0,0 +1,49 @@
+<?php
+$hd = mysqli_connect("localhost","itndata","itndata","itndata") or die("Error " . mysqli_error($hd));
+
+if ($argv[1] == "flush")
+{
+	mysqli_query($hd,"truncate table tmp_noms");
+	mysqli_query($hd,"truncate table tmp_vote");
+	
+}
+if ($argv[1] == "countrymap")
+{
+	mysqli_query($hd,"update tmp_noms t1 inner join countries t2 on t1.country = t2.country set t1.where_micro = t2.subregion");
+	mysqli_query($hd,"update tmp_noms t1 inner join countries t2 on t1.where_micro = t2.subregion set t1.where_macro = t2.region");
+	$sql = "select * from tmp_noms where where_macro = '' or where_macro is null";
+	$res = mysqli_query($hd,$sql);
+	while ($row = mysqli_fetch_assoc($res))
+	{
+		print_r($row);
+	}
+}
+
+if ($argv[1] == "delete")
+{
+	mysqli_query($hd,"delete from tmp_noms where country = 'delete'");
+
+}
+
+if ($argv[1] == 'backup')
+{
+	$stamp = date("Ymdhis");
+	$exec = "mysqldump -u itndata -pitndata itndata > backup/itndata.$stamp.sql";
+	echo "$exec\n";
+	exec($exec);
+}
+
+if ($argv[1] == 'pump')
+{
+	$stamp = date("Ymdhis");
+	$exec = "mysqldump -t -c -u itndata -pitndata itndata tmp_noms tmp_vote";
+	$output = Array();
+	exec($exec, $output);
+	$output = implode("\n",$output);
+	$output = "USE itndata;\n" . $output;
+	$output = str_replace("`tmp_vote`","`vote`",$output);
+	$output = str_replace("`tmp_noms`","`noms`",$output);
+	file_put_contents("backup/pump.sql",$output);
+	exec("mysql -u itndata -pitndata < backup/pump.sql");
+}
+
